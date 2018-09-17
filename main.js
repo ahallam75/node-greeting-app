08fa3f984db5f1592e5ffaeb317c1ad41d86aa4e
ahallam <alexhallam75@gmail.com>
2018-09-17 14:51:09 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+const goodbyes = require("./goodbye.js");
+
+// "Goodbye"
+goodbyes.goodbyeEnglish();
+
+// "Sayonara"
+goodbyes.goodbyeJapanese();
+
+// "Adios"
+goodbyes.goodbyeSpanish();
+
+// "Bless"
+goodbyes.goodbyeIcelandic();
